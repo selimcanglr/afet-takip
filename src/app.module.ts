89aa628f76e6cb44b008d1795aaa74b1f4244c2f
Selim Can Guler <selimcangler@gmail.com>
2023-02-18 15:49:01 +0300

    model initialization for beta version

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { ArticlesModule } from './articles/articles.module';
-import { SupplyModule } from './supply/supply.module';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule, SupplyModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
